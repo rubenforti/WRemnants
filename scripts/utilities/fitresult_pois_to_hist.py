bafb4ee6053de7d5e6bd31f1aa908afa0d7c4e17
David <david.walter@cern.ch>
2024-04-16 16:04:23 +0200

    Fix fitresult_pois_to_hist function

--- scripts/utilities/fitresult_pois_to_hist.py
+++ scripts/utilities/fitresult_pois_to_hist.py
@@ -19,10 +19,15 @@ logger = logging.setup_logger(__file__, args.verbose, args.noColorLogger)
 if not args.observed and not args.expected:
     raise IOError(f"Result from expected or observed fit must be specified with '--observed' or '--expected'")
 result = {}
+meta = None
+
 if args.observed:
     result, meta = fitresult_pois_to_hist(args.observed.replace(".root",".hdf5"), result, uncertainties=None)
 if args.expected:
     result, meta_exp = fitresult_pois_to_hist(args.expected.replace(".root",".hdf5"), result, uncertainties=None, expected=True)
+    if not args.observed:
+        meta = meta_exp
+        meta_exp = None
 
 # saving the result histograms
 outfile = f"{args.outfolder}/{args.outputFile}{ '.hdf5' if args.h5py else '.pkl'}"
@@ -38,8 +43,16 @@ if args.h5py:
     with h5py.File(outfile, "w") as f:
         logger.debug(f"Pickle and dump results")
         ioutils.pickle_dump_h5py("results", result, f)
-        ioutils.pickle_dump_h5py("meta", meta, f)
+        if meta is not None:
+            ioutils.pickle_dump_h5py("meta", meta, f)
+        if meta_exp is not None:
+            ioutils.pickle_dump_h5py("meta_exp", meta_exp, f)
 else:
     import pickle
     with open(outfile, "wb") as f:
-        pickle.dump({"results": result, "meta": meta}, f)
+        meta_info = {} 
+        if meta is not None:
+            meta_info["meta"] = meta
+        if meta_exp is not None:
+            meta_info["meta_exp"] = meta_exp
+        pickle.dump({"results": result, **meta_info}, f)
