abe0233f6e466e46491935c0e4a3cdd3278357a5
Kenneth Long <kdlong@hep.wisc.edu>
2024-09-14 11:18:47 +0200

    Changes to summary plot

--- scripts/plotting/make_results_summary.py
+++ scripts/plotting/make_results_summary.py
@@ -2,6 +2,7 @@ import argparse
 from utilities.io_tools import combinetf_input, input_tools, output_tools
 from utilities import common
 import matplotlib.pyplot as plt
+from matplotlib import ticker
 import pandas as pd
 from wremnants import plot_tools
 
@@ -10,14 +11,14 @@ parser.add_argument("-i", "--fitresult", type=str, required=True, help="fitresul
 args = parser.parse_args()
 
 dfw = pd.DataFrame.from_dict({
-    "Name" : ["LEP Combination", "D0 (Run 2)", "CDF (Run 2)", "LHCb", "ATLAS (2024)", "PDG Average"],
-    "value" : [80376, 80375, 80434, 80354, 80367, 80369.2],
+    "Name" : ["LEP Combination", "D0", "CDF", "LHCb", "ATLAS", "PDG Average"],
+    "value" : [80376, 80375, 80433.5, 80354, 80366.5, 80369.2],
     "err_total" : [33, 23, 9.4, 32, 15.9, 13.3],
     "err_stat" : [25, 11, 6.4, 23, 9.8, 13.3],
     "Reference" : ["Phys. Rep. 532 (2013) 119", "Phys. Rev. Lett. 108 (2012) 151804",
                   "Science 376 (2022) 6589", "JHEP 01 (2022) 036", "arxiv:2403.15085 Submitted to EPJC", 
                    "Eur.Phys.J.C 84 (2024) 5, 451"],
-    "color" : ["black"]*5+["gray"],
+    "color" : ["black"]*5+["navy"],
 })
 
 cms_res = combinetf_input.read_groupunc_df(args.fitresult, ["stat",], name="CMS")
@@ -32,34 +33,53 @@ if args.postfix:
 eoscp = output_tools.is_eosuser_path(args.outpath)
 outdir = output_tools.make_plot_dir(args.outpath, args.outfolder, eoscp=eoscp)
 
-fig = plot_tools.make_summary_plot(80353, 6, None,
-    dfw_cms,
+#offset=-0.25
+offset=0
+
+fig = plot_tools.make_summary_plot(80353, 6, "EW fit",
+    dfw_cms.loc[:,("Name", "value", "err_total")],
     colors=list(dfw_cms["color"]),
-    xlim=[80260, 80460],
-    xlabel="$m_{W}$ (MeV)", 
+    xlim=[80275, 80475],
+    xlabel=r"$\mathit{m}_{W}$ (MeV)", 
     out=outdir, 
     outfolder=args.outfolder,
     name=name,
-    legend_loc=None,
     capsize=6,
     width_scale=1.25,
     cms_label=args.cmsDecor,
+    cms_loc=0,
+    padding=4,
+    point_size=0.24,
+    #top_offset=offset,
+    #bottom_offset=offset*2,
+    label_points=False,
+    legend_loc='upper left',
+    legtext_size=18,
 )
 
 nentries = len(dfw_cms)
 
+top = nentries+0.5
+step = (top+0.25)/nentries
+
 ax = plt.gca()
+xpos = 80190
 for i,row in dfw_cms.iterrows():
     isCMS = row.loc["Name"] == "CMS" 
-    offset = nentries+0.4
-    pos = offset-i*1.1
-    ax.annotate(row.loc["Name"], (80170, pos), fontsize=18, ha="left", annotation_clip=False, color=row.loc["color"], weight=600)
-    ax.annotate(row.loc["Reference"], (80170, pos-0.3), fontsize=10, ha="left", color="dimgrey", annotation_clip=False, style='italic' if isCMS else None)
-    label = f"$m_{{W}}$ = {row.loc['value']:.0f} $\pm$ {round(row.loc['err_total'], 0):.0f}"
-    if row.loc["Name"] in ["CMS", "CDF"]:
-        label = f"$m_{{W}}$ = {row.loc['value']:.1f} $\pm$ {round(row.loc['err_total'], 1):.1f}"
-    ax.annotate(label, (80170, pos-0.6), fontsize=10, ha="left", color="dimgrey", annotation_clip=False)
+    pos = top-step*i
+    ax.annotate(row["Name"], (xpos, pos), fontsize=18, ha="left", annotation_clip=False, color=row.loc["color"], weight=600)
+    if row.loc["Name"] in ["CMS", "CDF", "ATLAS"]:
+        label = f"$m_{{W}}$ = {row.loc['value']:.1f} $\pm$ {round(row.loc['err_total'], 1):.1f} MeV"
+    else:
+        label = f"$m_{{W}}$ = {row.loc['value']:.0f} $\pm$ {round(row.loc['err_total'], 0):.0f} MeV"
+    ax.annotate(label, (xpos, pos-0.3), fontsize=10, ha="left", color="black", annotation_clip=False)
+    ax.annotate(row["Reference"], (xpos, pos-.6), fontsize=10, ha="left", color="dimgrey", annotation_clip=False, style='italic' if isCMS else None)
 
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.xaxis.set_major_locator(ticker.MultipleLocator(50))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(25))
+ax.xaxis.grid(False, which='both')
+ax.yaxis.grid(False, which='both')
 plot_tools.save_pdf_and_png(outdir, name, fig)
 plot_tools.write_index_and_log(outdir, name)
 if eoscp:
