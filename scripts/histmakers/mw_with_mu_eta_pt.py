388b6fba796a49349eb10b7039d221f58e71b49c
Marco Cipriani <marco.cipriani@cern.ch>
2025-01-14 12:33:18 +0100

    fix

--- scripts/histmakers/mw_with_mu_eta_pt.py
+++ scripts/histmakers/mw_with_mu_eta_pt.py
@@ -665,20 +665,21 @@ def build_graph(df, dataset):
         df = df.Filter(f"deltaPhiMuonMet > {dphiMuonMetCut}") # pi/4 was found to be a good threshold for signal with mT > 40 GeV
 
     df = df.Define("passMT", f"transverseMass >= {mtw_min}")
-
-    # more tests with fakes
-    axis_ptMinusMet = hist.axis.Regular(100, -50, 50, name = "ptMinusMet", overflow=True, underflow=True)
-    axis_mt_coarse = hist.axis.Regular(24, 0., 120., name = "mt", underflow=False, overflow=True)
-    axis_eta_coarse = hist.axis.Regular(12, -2.4, 2.4, name = "eta", underflow=False, overflow=False)
-
-    df = df.Define("ptMinusMet", "goodMuons_pt0 - MET_corr_rec_pt")
-    results.append(df.HistoBoost("testFakesMultiVar", [axis_eta_coarse, axis_pt, axis_charge, axis_mt_coarse, axis_passIso, axis_dphi_fakes, axis_ut_analysis, axis_ptMinusMet], ["goodMuons_eta0", "goodMuons_pt0", "goodMuons_charge0", "transverseMass", "passIso", "deltaPhiMuonMet", "goodMuons_angleSignUt0", "ptMinusMet", "nominal_weight"]))
-
-    axis_ut = hist.axis.Regular(70, -40., 100., name = "ut", underflow=True, overflow=True)
-    df = df.Define("goodMuons_utReco", "wrem::zqtproj0(goodMuons_pt0, goodMuons_phi0, MET_corr_rec_pt, MET_corr_rec_phi)")
-    results.append(df.HistoBoost("uTforPlots", [axis_eta_coarse, axis_pt, axis_charge, axis_mt_coarse, axis_passIso, axis_ut], ["goodMuons_eta0", "goodMuons_pt0", "goodMuons_charge0", "transverseMass", "passIso", "goodMuons_utReco", "nominal_weight"]))
     
     if auxiliary_histograms:
+
+        # more tests with fakes
+        axis_ptMinusMet = hist.axis.Regular(100, -50, 50, name = "ptMinusMet", overflow=True, underflow=True)
+        axis_mt_coarse = hist.axis.Regular(24, 0., 120., name = "mt", underflow=False, overflow=True)
+        axis_eta_coarse = hist.axis.Regular(12, -2.4, 2.4, name = "eta", underflow=False, overflow=False)
+        
+        df = df.Define("ptMinusMet", "goodMuons_pt0 - MET_corr_rec_pt")
+        results.append(df.HistoBoost("testFakesMultiVar", [axis_eta_coarse, axis_pt, axis_charge, axis_mt_coarse, axis_passIso, axis_dphi_fakes, axis_ut_analysis, axis_ptMinusMet], ["goodMuons_eta0", "goodMuons_pt0", "goodMuons_charge0", "transverseMass", "passIso", "deltaPhiMuonMet", "goodMuons_angleSignUt0", "ptMinusMet", "nominal_weight"]))
+        
+        axis_ut_fine = hist.axis.Regular(70, -40., 100., name = "ut", underflow=True, overflow=True)
+        df = df.Define("goodMuons_utReco", "wrem::zqtproj0(goodMuons_pt0, goodMuons_phi0, MET_corr_rec_pt, MET_corr_rec_phi)")
+        results.append(df.HistoBoost("uTforPlots", [axis_eta_coarse, axis_pt, axis_charge, axis_mt_coarse, axis_passIso, axis_ut_fine], ["goodMuons_eta0", "goodMuons_pt0", "goodMuons_charge0", "transverseMass", "passIso", "goodMuons_utReco", "nominal_weight"]))
+
         # control plots, lepton, met, to plot them later (need eta-pt to make fakes)
         results.append(df.HistoBoost("leptonPhi", [axis_phi, *axes_fakerate], ["goodMuons_phi0", *columns_fakerate, "nominal_weight"]))
         results.append(df.HistoBoost("MET", [axis_met, *axes_fakerate], ["MET_corr_rec_pt", *columns_fakerate, "nominal_weight"]))
