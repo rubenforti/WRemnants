ac821ba2fd07fc5b8e66a76c8a4d8d60db0bba79
Ruben Forti <rubenforti27@gmail.com>
2025-01-20 10:31:31 +0100

    helpers update to run on 2017-2018

--- wremnants/datasets/dataset_tools.py
+++ wremnants/datasets/dataset_tools.py
@@ -232,6 +232,7 @@ def getDatasets(
     oneMCfileEveryN=None,
     checkFileForZombie=False,
     era="2016PostVFP",
+    eraDataSel=None, #select specific eras for data
     extended=True,
 ):
 
@@ -284,6 +285,10 @@ def getDatasets(
             base_path = base_path.replace("NanoAOD", "NanoGen")
 
         is_data = info.get("group", "") == "Data"
+
+        if is_data and eraDataSel:
+            all_filepaths = info["filepaths"]
+            info["filepaths"] = [f for f in all_filepaths if any(f"Run{era}{e_sel}" in f for e_sel in eraDataSel)]
 
         prod_tags = data_tags if is_data else mc_tags
         nfiles = maxFiles
